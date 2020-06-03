d1f2a148e1f86adbff18ab0f310633d789f165ee
arunmca111 <39898464+arunmca111@users.noreply.github.com>
2020-06-03 10:09:29 +0530

    rest api setup

--- piCode/resp-api-cloud/AADPVendCloudRest/src/main/java/com/aadp/vend/ui/contoller/UserController.java
+++ piCode/resp-api-cloud/AADPVendCloudRest/src/main/java/com/aadp/vend/ui/contoller/UserController.java
@@ -1,5 +1,11 @@
 package com.aadp.vend.ui.contoller;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +13,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 	
-	
+
+@GetMapping
+public String getUser() {
+	return "get user called";
+}	
+@PostMapping
+public String createUser() {
+	return "create user called";
+}
+
+@PutMapping
+public String updateUser() {
+	return "update user called";
+}
+
+@DeleteMapping
+public String deleteUser() {
+	return "delete user called";
+}
 
 }
